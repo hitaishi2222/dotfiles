7363f3fef3aab178623ce14fcf8ad3dcfd0b2674
Hitaishi <38245041+hitaishi2222@users.noreply.github.com>
2024-08-29 04:38:00 +0530

    Update colourscheme.lua

--- nvim/lua/hiti/plugins/colourscheme.lua
+++ nvim/lua/hiti/plugins/colourscheme.lua
@@ -22,7 +22,7 @@ return {
           colors.bg_highlight = bg_highlight
           colors.bg_popup = bg_dark
           colors.bg_search = bg_search
-          colors.bg_sidebar = "#071205"
+          colors.bg_sidebar = bg_dark
           colors.bg_statusline = bg_dark
           colors.bg_visual = bg_visual
           colors.border = border
@@ -30,6 +30,7 @@ return {
           colors.fg_dark = fg_dark
           colors.fg_float = fg
           colors.fg_gutter = fg_gutter
+          colors.fg_sidebar = fg_dark
         end,
       })
       -- load the colorscheme here

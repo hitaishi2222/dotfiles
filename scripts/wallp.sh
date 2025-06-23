5866976bd74098810c91a71aa633a5f2f0c9a328
hitaishi2222 <hitaishi2222@gmail.com>
2025-06-23 18:46:10 +0530

    added waybar config

--- scripts/wallp.sh
+++ scripts/wallp.sh
@@ -5,10 +5,10 @@ WALLPAPER_DIR="$HOME/Pictures/HD_wallpapers/"
 
 # Get the list of files
 SELECTED=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' \) \
-    | fzf --style full --preview 'chafa {}' --preview-window=up:80% --layout=reverse)
+    | fzf --preview 'chafa -f symbols --colors truecolor {}' --preview-window=up:70%)
 
 # If a file was selected, set it as wallpaper
 if [ -n "$SELECTED" ]; then
-    swww img "$SELECTED" --transition-type grow --transition-duration 0.7
+    hyprctl hyprpaper reload DP-6,$SELECTED
     wallust run $SELECTED
 fi
